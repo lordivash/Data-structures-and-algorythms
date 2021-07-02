f1c9ba0d6bfdb658034f2c5b65046c00e533049f
lordivash <55952043+lordivash@users.noreply.github.com>
2021-07-03 02:17:22 +0300

    Add files via upload

---
+++ read_file_to_frequency_dict.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+	map <string, int> word_frequency;
+
+	ifstream file("input.txt");
+
+	while (!file.eof()) {
+
+		string word;
+		file >> word;
+
+		if (word != "") {
+
+			if (word_frequency.find(word) == word_frequency.end()) {
+				word_frequency[word] = 0;
+			}
+
+			cout << word_frequency[word]++ << " ";
+		}
+
+	}
+
+	return 0;
+}
---
+++ the_most_frequent_word.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+
+	map <string, int> word_frequency;
+
+	ifstream file("input.txt");
+
+	string most_frequent_word = "";
+
+	//Возможно следует предусмотреть случай пустой строки/файла
+	file >> most_frequent_word;
+	word_frequency[most_frequent_word] = 1;
+
+	while (!file.eof()) {
+
+		string word;
+		file >> word;
+
+		if (word != "") {
+
+			if (word_frequency.find(word) == word_frequency.end()) {
+				word_frequency[word] = 0;
+			}
+
+			word_frequency[word]++;
+
+			if (word_frequency[word] > word_frequency[most_frequent_word]) {
+
+				most_frequent_word = word;
+
+			}
+			else if (word_frequency[word] == word_frequency[most_frequent_word] && word < most_frequent_word) {
+
+				most_frequent_word = word;
+
+			}
+
+		}
+	}
+
+	cout << most_frequent_word;
+
+	return 0;
+}
